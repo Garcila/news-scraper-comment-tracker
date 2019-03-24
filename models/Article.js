2cf9aecaca86c291c1516173c80835ba3b6d9a7c
garcila <germanarcila@gmail.com>
2019-03-24 14:44:41 -0400

    modify post/articles/:id to push a comment to the comments array, and also modify the Comment.js model to store an array instead of a single comment

--- models/Article.js
+++ models/Article.js
@@ -1,35 +1,37 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   link: {
     type: String,
-    required: true
+    required: true,
   },
   summary: {
     type: String,
-    required: true
+    required: true,
   },
   photo: {
     type: String,
-    required: true
+    required: true,
   },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
-  comment: {
-    type: Schema.Types.ObjectId,
-    ref: "Comment"
-  }
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+var Article = mongoose.model('Article', ArticleSchema);
 
 // Export the Article model
 module.exports = Article;
--- server.js
+++ server.js
@@ -90,78 +90,12 @@ app.get("/articles/:id", function(req, res) {
     });
 });
 
-//   // First, we grab the body of the html with axios
-//   axios.get("http://www.echojs.com/").then(function(response) {
-//     // Then, we load that into cheerio and save it to $ for a shorthand selector
-//     var $ = cheerio.load(response.data);
 
-//     // Now, we grab every h2 within an article tag, and do the following:
-//     $("article h2").each(function(i, element) {
-//       // Save an empty result object
-//       var result = {};
-
-//       // Add the text and href of every link, and save them as properties of the result object
-//       result.title = $(this)
-//         .children("a")
-//         .text();
-//       result.link = $(this)
-//         .children("a")
-//         .attr("href");
-
-//       // Create a new Article using the `result` object built from scraping
-//       db.Article.create(result)
-//         .then(function(dbArticle) {
-//           // View the added result in the console
-//           console.log(dbArticle);
-//         })
-//         .catch(function(err) {
-//           // If an error occurred, log it
-//           console.log(err);
-//         });
-//     });
-
-//     // Send a message to the client
-//     res.send("Scrape Complete");
-//   });
-// });
-
-// Route for getting all Articles from the db
-// app.get("/articles", function(req, res) {
-//   // TODO: Finish the route so it grabs all of the articles
-//   db.Article.find({})
-//   .then(function (articles){
-//     res.json(articles);
-//   });
-// });
-
-// // Route for grabbing a specific Article by id, populate it with it's note
-// app.get("/articles/:id", function(req, res) {
-//   db.Article.findOne({_id: req.params.id})
-//     .populate("note")
-//     .then(function(dbArticle){
-//       res.json(dbArticle)
-//     })
-//     .catch(function(err){
-//       res.json(err);
-//     });
-//   // TODO
-//   // ====
-//   // Finish the route so it finds one article using the req.params.id,
-//   // and run the populate method with "note",
-//   // then responds with the article with the note included
-// });
-
-/*
-// Route for saving/updating an Article's associated Note
+// Route for saving/updating an Article's associated Comment
 app.post("/articles/:id", function(req, res) {
-  // TODO
-  // ====
-  // save the new note that gets posted to the Notes collection
-  // then find an article from the req.params.id
-  // and update it's "note" property with the _id of the new note
-  db.Note.create(req.body)
-  .then(function(dbNote) {
-    return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+  db.Comment.create(req.body)
+  .then(function(dbComment) {
+    return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: {comments: dbComment._id} }, { new: true });
   })
   .then(function(dbUser) {
     res.json(dbUser);
@@ -170,7 +104,7 @@ app.post("/articles/:id", function(req, res) {
     res.json(err);
   });
 });
-*/
+
 
 // Start the server
 app.listen(PORT, function() {
